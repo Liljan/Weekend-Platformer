db9d7ec6c8f1eeca00c93e40b339f3e01e781793
Liljan <marcus.t.lilja@gmail.com>
2018-03-03 17:51:44 +0100

    Pixel perfect camera.

---
+++ Weekend-Platformer/Assets/Scripts/Gameplay/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+    private Camera camera;
+    public Transform target;
+
+    private void Awake()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Vector3 = camera.scree
+
+        float x = target.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+
+
+        transform.position = new Vector3(x,y,z);
+	}
+}
