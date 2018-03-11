0d189b53e668a48b523af169e80af4e1ae559245
Liljan <marcus.t.lilja@gmail.com>
2018-03-11 19:11:35 +0100

    Level Loader.

---
+++ Weekend-Platformer/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public string sceneName = "Dev_Gameplay_Scene";
+    public Slider slider;
+    public Text text;
+
+    public Animator sliderAnimator;
+
+    private void Start()
+    {
+        LoadLevel(sceneName);
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadLevelAsync(sceneName));
+    }
+
+    IEnumerator LoadLevelAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        bool isLoaded = false;
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            if(operation.progress >= 0.9f)
+            {
+                if(!isLoaded)
+                {
+                    // set Text
+                    sliderAnimator.SetTrigger("Done");
+                    isLoaded = true;
+                }
+
+                if(Input.GetButtonDown("Enter"))
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }
+    }
+}
